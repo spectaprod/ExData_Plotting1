47fcd8ae49924cc2ac35255ad1b10a525db3d465
spectaprod <timm@daveramsey.com>
2016-10-09 13:49:20 -0500

    Commit4
    Updated libraries and location in script

--- PlottingScripts.R
+++ PlottingScripts.R
@@ -1,7 +1,8 @@
+library(lubridate)
+library(dplyr)
 ## read data
 d <- read.table("household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
 ## change Date from factor to date and filter for 2007-02-01 - 2007-02-02
-library(lubridate)
 d$Date <- dmy(d$Date)
 d1<- filter(d,Date == "2007-02-01" | Date == "2007-02-02")
 d1$timestamp <- strptime(paste(d1$Date,d1$Time),format = "%Y-%m-%d %H:%M:%S")
